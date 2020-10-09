aa00c343578b629b77c70cff361db402c74526b1
DivaniNL <dylanvannierop@gmail.com>
2020-10-09 20:56:37 +0200

    nice commit comment for all my changes

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -25,7 +25,7 @@ const KEY = {
    const ROAD = {
     LENGTH: { NONE: 0, SHORT: 25, MEDIUM: 50, LONG: 100 }, // num segments
     HILL: { NONE: 0, LOW: 20, MEDIUM: 40, HIGH: 60 },
-    CURVE: { NONE: 0, EASY: 2, MEDIUM: 4, HARD: 6 },
+    CURVE: { NONE: 0, EASY: 2, MEDIUM: 4, HARD: 5 },
    };
    const SPRITES = {
     BIKE: [
